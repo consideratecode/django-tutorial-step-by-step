9f53885a35d483dd37f03142e5870933a203f4fd
Daniel Hepper <daniel.hepper@epicco.de>
2017-12-11 12:53:46 +0100

    Part 1: Writing your first view
    https://docs.djangoproject.com/en/2.0/intro/tutorial01/#write-your-first-view

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
