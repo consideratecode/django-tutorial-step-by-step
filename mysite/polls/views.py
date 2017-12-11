e20830bc84e8218fd2e556e3a1220d06d6725c8e
Daniel Hepper <daniel.hepper@epicco.de>
2017-12-11 13:09:00 +0100

    Part 3: Write views that actually do something - A shortcut: render()
    https://docs.djangoproject.com/en/2.0/intro/tutorial03/#a-shortcut-render

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,13 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
