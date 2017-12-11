50536f3edf546cb07e8d63a13ce5e525a63f551a
Daniel Hepper <daniel.hepper@epicco.de>
2017-12-11 13:10:16 +0100

    Part 3: Raising a 404 error
    https://docs.djangoproject.com/en/2.0/intro/tutorial03/#raising-a-404-error

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -10,7 +11,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
