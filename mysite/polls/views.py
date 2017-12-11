72dfc6f4538f2878af1c3af11e25b0c6b1954647
Daniel Hepper <daniel.hepper@epicco.de>
2017-12-11 13:08:11 +0100

    Part 3: Write views that actually do something - with templates
    https://docs.djangoproject.com/en/2.0/intro/tutorial03/#write-views-that-actually-do-something

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
