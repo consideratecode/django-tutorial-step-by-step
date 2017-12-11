ec4250c650d6ee5be3af2f98f736a68455808dcd
Daniel Hepper <daniel.hepper@epicco.de>
2017-12-11 13:04:47 +0100

    Part 3: Writing more views
    https://docs.djangoproject.com/en/2.0/intro/tutorial03/#writing-more-views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
