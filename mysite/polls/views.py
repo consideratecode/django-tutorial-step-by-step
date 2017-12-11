049aada544fd4d304e9b02c32bddf3e0b2b4e41f
Daniel Hepper <daniel.hepper@epicco.de>
2017-12-11 13:06:41 +0100

    Part 3: Write views that actually do something
    https://docs.djangoproject.com/en/2.0/intro/tutorial03/#write-views-that-actually-do-something

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
