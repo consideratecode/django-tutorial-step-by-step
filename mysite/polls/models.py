76cba2a2780ae2a0ae5321cc38b65f06777a134c
Daniel Hepper <daniel.hepper@epicco.de>
2017-12-13 14:44:51 +0100

    Part 5: Writing our first test - Fixing the bug
    https://docs.djangoproject.com/en/2.0/intro/tutorial05/#fixing-the-bug

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -12,7 +12,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
