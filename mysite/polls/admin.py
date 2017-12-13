06d8f253da3c5e92cd7f4b69a1c25d9fe6e3203f
Daniel Hepper <daniel.hepper@epicco.de>
2017-12-13 15:22:11 +0100

    Part 7: Adding related objects - use TabularInline (instead of StackedInline)
    https://docs.djangoproject.com/en/2.0/intro/tutorial07/#adding-related-objects

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
