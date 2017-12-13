db80e27a54476edd390b0073e5dc8e9ea20df8f6
Daniel Hepper <daniel.hepper@epicco.de>
2017-12-13 15:36:53 +0100

    Part 7: Customize the admin change list - add search_fields
    https://docs.djangoproject.com/en/2.0/intro/tutorial07/#customize-the-admin-change-list

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -16,5 +16,6 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
