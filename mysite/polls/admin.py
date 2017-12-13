3ff25678d47efbd68ff03a2de48f5cb9cb5aab11
Daniel Hepper <daniel.hepper@epicco.de>
2017-12-13 15:19:39 +0100

    Part 7: Customize the admin form - split the form
    https://docs.djangoproject.com/en/2.0/intro/tutorial07/#customize-the-admin-form

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,6 +4,9 @@ from .models import Question
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
 
 admin.site.register(Question, QuestionAdmin)
