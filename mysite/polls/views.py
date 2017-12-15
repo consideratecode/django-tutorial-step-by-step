d9494c080825a3a43182ff1b7087fc106a1479fd
Daniel Hepper <daniel.hepper@epicco.de>
2017-12-15 19:35:39 +0100

    Part 4: Write a simple form - results view
    https://docs.djangoproject.com/en/2.0/intro/tutorial04/#write-a-simple-form

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -15,8 +15,8 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
