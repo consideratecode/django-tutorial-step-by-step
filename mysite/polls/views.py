a618507648e365865d0c78643adcf68338edfa02
Daniel Hepper <daniel.hepper@epicco.de>
2017-12-15 19:17:45 +0100

    Part 3: Raising a 404 error - A shortcut: get_object_or_404()
    https://docs.djangoproject.com/en/2.0/intro/tutorial03/#a-shortcut-get-object-or-404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -11,10 +10,7 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
